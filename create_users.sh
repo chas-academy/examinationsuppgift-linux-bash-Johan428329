e43272fc7f4b0c6c6025063091785b157d9dc6bb
Johan Persson <johanpersson070@gmail.com>
2026-05-19 14:13:12 +0200

    Refactor user creation script for improved structure

--- create_users.sh
+++ create_users.sh
@@ -1,74 +1,55 @@
 #!/bin/bash
 
-# kontrollera att vi kör som root
+# kontroller root
 if [[ $EUID -ne 0 ]]; then
-    echo "Du måste vara root för att köra det här scriptet"
+    echo "Fel: scriptet måste köras som root"
     exit 1
 fi
 
-# kolla att vi har argument
+# minst en användare som argument
 if [[ $# -eq 0 ]]; then
-    echo "Användning: $0 user1 [user2 user3 ...]"
+    echo "Användning: $0 användarnamn1 [användarnamn2 ...]"
     exit 1
 fi
 
-# räkna skapade användare
-users_created=0
+folders=("Documents" "Downloads" "Work")
 
-# loopa genom användarnamnen
+# loop som skapar alla användare och sätter upp katalogstruktur
+
 for username in "$@"; do
-    # skapa användaren om den inte finns
+	# ifall anvädare med hemkatalog inte finns, så skapades det
     if ! id "$username" &>/dev/null; then
         useradd -m "$username"
-        if [ $? -eq 0 ]; then
-            ((users_created++))
-        fi
-    else
-        # redan befintlig användare
-        ((users_created++))
     fi
-    
-    # hämta hemkatalog
-    homedir=$(getent passwd "$username" | cut -d: -f6)
-    
-    # hoppa över om hemkatalog inte finns
-    if [ -z "$homedir" ]; then
-        continue
-    fi
-    
-    # skapa mapparna
-    mkdir -p "$homedir/Documents"
-    mkdir -p "$homedir/Downloads"
-    mkdir -p "$homedir/Work"
-    
-    # sätt rättigheter (700 = bara ägaren)
-    chmod 700 "$homedir/Documents"
-    chmod 700 "$homedir/Downloads"
-    chmod 700 "$homedir/Work"
-    
-    # användar äger sina mappar
-    chown "$username:$username" "$homedir/Documents"
-    chown "$username:$username" "$homedir/Downloads"
-    chown "$username:$username" "$homedir/Work"
-    
-    # hämta alla systemanvändare
-    users=$(cut -d: -f1 /etc/passwd | grep -v "^$" | sort)
-    
-    # skapa välkomstfilen
-    wfile="$homedir/welcome.txt"
+
+	# skapa undermappar i hemkatalog
+    for folder in "${folders[@]}"; do
+        mkdir -p "/home/$username/$folder"
+    done
+
+    # korrekt behörighet till mappen
+    chown -R "$username:$username" "/home/$username"
+
+    # sätta rättigheter ill ägaren (700)
+    chmod 700 "/home/$username"
+    chmod 700 "/home/$username/Documents"
+    chmod 700 "/home/$username/Downloads"
+    chmod 700 "/home/$username/Work"
+done
+
+# andra loopen som skapad welcome.txt, då alla användare finns i system.
+for username in "$@"; do
+    wfile="/home/$username/welcome.txt"
+
+	# personalizerad meddelande för välkomst
     echo "Välkommen $username" > "$wfile"
     echo "" >> "$wfile"
     echo "alla användare i systemet:" >> "$wfile"
-    echo "$users" >> "$wfile"
-    
-    # rättigheter på welcome-filen
-    chmod 600 "$wfile"
-    chown "$username:$username" "$wfile"
-done
 
-# avslut med rätt kod
-if [ $users_created -gt 0 ]; then
-    exit 0
-else
-    exit 1
-fi
+    # lista alla användare i systemet
+    cut -d: -f1 /etc/passwd | sort >> "$wfile"
+
+    # rättigheter så att endast ägaren ska kunna läsa
+    chown "$username:$username" "$wfile"
+    chmod 600 "$wfile"
+done
